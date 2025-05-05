9aa22ca7e66a3cb7691c0c10e9ab6c8da2922eb9
Nathan OGER <nathiii045@gmail.com>
2025-05-05 14:58:08 +0200

    init react + vite + shadcn ui - fonctionnel

--- scFront/src/App.tsx
+++ scFront/src/App.tsx
@@ -1,18 +1,11 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
-import { Button } from "./components/ui/button";
-
+import { ModeToggle } from "./components/ui/mode-toggle";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex flex-col items-center justify-center min-h-screen gap-4 p-8">
-        <h1 className="text-3xl font-bold">Test Mode Sombre</h1>
-        <div className="flex items-center gap-4">
-          <ModeToggle />
-          <Button variant="default">Bouton Test</Button>
-          <Button variant="secondary">Bouton Secondaire</Button>
-        </div>
+      <div className="flex h-screen w-screen items-center justify-center bg-background">
+        <ModeToggle />
       </div>
     </ThemeProvider>
   );
