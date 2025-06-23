ad38958d4ea4fab678426a8f7e72e62f59e8000e
Anas <anasdaoui2000@gmail.com>
2025-06-23 15:17:48 +0200

    correction des error de build du front

--- scFront/src/App.tsx
+++ scFront/src/App.tsx
@@ -1,11 +1,8 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
-import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import { Home } from "./pages/home/Home";
-import { ModeToggle } from "./components/mode-toggle";
-import { Button } from "./components/ui/button";
 import Dashboard from "./pages/dashboard/Dashboard";
 import AppLayout from "./layout/AppLayout";
 
